f5efb3f1365a3fb83dcc33f6a4c9219037c7e142
factcondenser <markpan@live.com>
2018-05-17 00:38:34 -0500

    Add phantomjs/poltergeist capability

--- check_amberbit.rb
+++ check_amberbit.rb
@@ -1,9 +1,14 @@
-require 'capybara'
-session = Capybara::Session.new(:selenium)
-session.visit "http://www.amberbit.com"
+session = if ARGV[0] != 'phantomjs'
+  require 'capybara'
+  Capybara::Session.new(:selenium)
+else
+  require 'capybara/poltergeist'
+  Capybara::Session.new(:poltergeist)
+end
 
-puts(session)
-if session.has_content?("Ruby on Rails")
+session.visit "https://www.amberbit.com/blog/2014/2/12/automate-tasks-on-the-web-with-ruby-and-capybara/"
+
+if session.has_content?("Ruby on Rails web development")
   puts "All shiny, captain!"
 else
   puts ":( no tagline fonud, possibly something's broken"
